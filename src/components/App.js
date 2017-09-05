3c7bd34459011aad7858c3a0e67457da55f583d5
sgsg13 <mesa5150@gmail.com>
2017-09-05 14:13:01 +0300

    add menu add stepper

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,13 @@
 import React, {Component} from 'react';
-import { Grid, Row, Col} from 'react-bootstrap'
-import AppBar from 'material-ui/AppBar';
-
+import NavBar from './NavBar';
+import StepperContent from './StepperContent'
 
 class App extends Component {
     render() {
         return (
             <div>
-                <AppBar
-                    title=""
-                    iconClassNameRight="muidocs-icon-navigation-expand-more"
-                />
-                <Grid>
-                    <Row>
-                        <Col md={12}>
-                        </Col>
-                    </Row>
-                </Grid>
+               <NavBar/>
+                <StepperContent/>
             </div>
 
         );
---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    handleToggle = () => this.setState({isOpen: !this.state.isOpen});
+
+    handleClose = () => this.setState({isOpen: false});
+
+    render() {
+        return (
+            <div>
+                <AppBar
+                    title="SG13 Studio"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    onLeftIconButtonTouchTap = {this.handleToggle}
+                />
+                <Drawer
+                    docked={false}
+                    width={200}
+                    open={this.state.isOpen}
+                    onRequestChange={(isOpen) => this.setState({isOpen})}
+                >
+                    <MenuItem onClick={this.handleClose}>Главная</MenuItem>
+                    <MenuItem onClick={this.handleClose}>Фотогалерея</MenuItem>
+                    <MenuItem onClick={this.handleClose}>О нас</MenuItem>
+                    <MenuItem onClick={this.handleClose}>Контакты</MenuItem>
+                </Drawer>
+                
+            </div>
+
+        );
+    }
+}
+
+
+export default NavBar;
